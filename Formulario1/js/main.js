e84364210dbdda0bc541efb50c7d21cffe11db20
DouglasDias <ddc_1996@hotmail.com>
2024-04-12 17:15:05 -0300

    first commit

--- Formulario1/js/main.js
+++ Formulario1/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $('#telefone').mask('(33) 99999-9999',{
-        placeholder:'(33) 99999-9999'
+    $('#telefone').mask('(00) 00000-0000',{
+        placeholder:'(00) 00000-0000'
     })
 
     $('#cpf').mask('000.000.000-00', {
